3b23d5516fc89c9aa383b27ebe020d9154e3c7f4
Saief El Gebali <saiefelsayed@gmail.com>
2022-03-26 15:29:31 +0000

    ADD: ASP.NET Core Web API Project

---
+++ SolarPanels.API/Controllers/SolarPanelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarPanels.Core.Algorithms;
+using SolarPanels.Core.Algorithms.Models;
+
+namespace SolarPanels.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SolarPanelController : Controller
+    {
+        [HttpPost]
+        public IEnumerable<EstimatedQuote> GetEstimatedQuotes(HouseSpecifications specs)
+        {
+            return QuoteEstimator.GetQuotes(specs);
+        }
+    }
+}
