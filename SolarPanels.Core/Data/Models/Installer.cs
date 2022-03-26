49b27e8752f68e0fb1d565b3741f86851ef5945c
Saief El Gebali <saiefelsayed@gmail.com>
2022-03-26 15:28:49 +0000

    FIX: Installer data to properties

--- SolarPanels.Core/Data/Models/Installer.cs
+++ SolarPanels.Core/Data/Models/Installer.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SolarPanels.Core.Data.JSON;
+﻿using SolarPanels.Core.Data.JSON;
 
 namespace SolarPanels.Core.Data.Models
 {
     public class Installer
     {
-        public readonly string Id;
-        public readonly double CallOutCost;
-        public readonly double CostPerPanel;
+        public string Id { get; private set; }
+        public double CallOutCost { get; private set; }
+        public double CostPerPanel { get; private set; }
 
         public Installer() { }
 
